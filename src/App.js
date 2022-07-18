bde5db03800fd8ad8f9b766f1adac2bfcd80c0b7
nyirurugo omar <nyirurugoomar@gmail.com>
2022-07-18 22:37:33 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import { ThemeProvider } from "./context/ThemeContext";
+
+function App() {
+  return (
+    <ThemeProvider>
+      <Navbar/>
+      
+    </ThemeProvider>
+  );
+}
+
+export default App;
