77d0818b3a4980b356128ea2f17883d04b053f75
nyirurugo omar <nyirurugoomar@gmail.com>
2022-07-23 15:07:17 +0200

    searching input style

--- src/components/CoinSearch.jsx
+++ src/components/CoinSearch.jsx
@@ -7,11 +7,15 @@ const CoinSearch = ({coins}) => {
     const [SearchText,setSearchText] = useState('')
     // console.log(coins )
   return (
-    <div>
-         <div>
-             <h1>Search Crypto</h1>
+    <div className='rounded-div my-4'>
+         <div className='flex flex-col md:flex-row justify-between pt-4 pb-6 text-center md:text-right'>
+             <h1 className='text-3xl font-bold my-2'>Search Crypto</h1>
              <form>
-                 <input onChange={(e)=>setSearchText(e.target.value) } type='text' placeholder='Search a coin' />
+                 <input 
+                 onChange={(e)=>setSearchText(e.target.value)} 
+                 className='w-full bg-primary border border-input px-4 py-2 rounded-2xl shadow-xl '  
+                 type='text' 
+                 placeholder='Search a coin' />
              </form>
          </div>
          <table>
